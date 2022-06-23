53af08741f90a1de0fb8088bbfb464301bdb077d
Shadman Syed <shadman3d@gmail.com>
2022-06-23 13:18:23 -0400

    Make each connection on a new thread

--- src/server.rs
+++ src/server.rs
@@ -8,60 +8,62 @@ async fn main() {
     let listener = TcpListener::bind("192.168.0.34:8080").await.unwrap();
     loop {
         let (mut socket, _) = listener.accept().await.unwrap();
-        let (read, mut writer) = socket.split();
-        let mut reader = BufReader::new(read);
+        tokio::spawn(async move {
+            let (read, mut writer) = socket.split();
+            let mut reader = BufReader::new(read);
 
-        // let mut data = BytesMut::new();
-        let mut data = BytesMut::new();
-        let mut message = String::new();
+            // let mut data = BytesMut::new();
+            let mut data = BytesMut::new();
+            let mut message = String::new();
 
 
-        match reader.read_line(&mut message).await {
-            Ok(bytes) => println!("bytes read: {}", bytes),
-            Err(e) => {
-                eprintln!("Malformed Responce: {}\n Skipping Request", e);
-                writer.write(&[2u8]).await.unwrap();
-                continue;
-            },
-        };
+            match reader.read_line(&mut message).await {
+                Ok(bytes) => println!("bytes read: {}", bytes),
+                Err(e) => {
+                    eprintln!("Malformed Responce: {}\n Skipping Request", e);
+                    writer.write(&[2u8]).await.unwrap();
+                    return;
+                },
+            };
 
-        println!("{:?}", message);
+            println!("{:?}", message);
 
-        let message = serde_json::from_str::<Message>(&message.trim()).unwrap();
+            let message = serde_json::from_str::<Message>(&message.trim()).unwrap();
 
-        let mut file = File::create(message.file_name).await.unwrap();
+            let mut file = File::create(message.file_name).await.unwrap();
 
-        
-        writer.write(&[1u8]).await.unwrap();
-
-        match message.status {
-            Status::Single => {    //Files under 64 bytes 
-                let bytes = reader.read_buf(&mut data).await.unwrap();
-                println!("{:?}, {}", data, bytes);
             
-                file.write_all(&mut data).await.unwrap();
-            },
-            Status::Multiple => { //Files over 64 bytes
-                'outer: loop {
-                    for _ in 0..message.packets {
-                        let mut chunk = [0u8; 64];
-                        match reader.read_exact(&mut chunk).await {
-                            Ok(_) => {},
-                            Err(err) => {
-                                eprintln!("{}", err);
-                                writer.write_all(&[3u8]).await.unwrap();
-                                continue 'outer;
-                            },
-                        };
-                        data.extend_from_slice(&chunk);
-                    }
-                    writer.write_all(&[1u8]).await.unwrap();
-                    break;
-                }
+            writer.write(&[1u8]).await.unwrap();
 
-                file.write_all(&mut data).await.unwrap();
-            },
-            Status::Ready => {},
-        }
+            match message.status {
+                Status::Single => {    //Files under 64 bytes 
+                    let bytes = reader.read_buf(&mut data).await.unwrap();
+                    println!("{:?}, {}", data, bytes);
+                
+                    file.write_all(&mut data).await.unwrap();
+                },
+                Status::Multiple => { //Files over 64 bytes
+                    'outer: loop {
+                        for _ in 0..message.packets {
+                            let mut chunk = [0u8; 64];
+                            match reader.read_exact(&mut chunk).await {
+                                Ok(_) => {},
+                                Err(err) => {
+                                    eprintln!("{}", err);
+                                    writer.write_all(&[3u8]).await.unwrap();
+                                    continue 'outer;
+                                },
+                            };
+                            data.extend_from_slice(&chunk);
+                        }
+                        writer.write_all(&[1u8]).await.unwrap();
+                        break;
+                    }
+
+                    file.write_all(&mut data).await.unwrap();
+                },
+                Status::Ready => {},
+            }
+        });
     }
 }
